b54b7d6e4979fb62c4c975aa430ffb7365288266
sweetmate-dev <john.amuesi525@gmail.com>
2018-12-12 08:27:11 +0000

    Initial Commit

--- src/redux/actions/home.js
+++
@@ -1,5 +0,0 @@
-import * as types from './types';
-
-export const welcome = () => ({
-  type: types.WELCOME,
-});
--- src/theme/colorStyles.js
+++
@@ -1,5 +0,0 @@
-export default {
-  PRIMARY_COLOR: 'blue',
-  SECONDARY_COLOR: '#034',
-  BACKGROUND_COLOR_LIGHT: '#f0f6f7',
-};

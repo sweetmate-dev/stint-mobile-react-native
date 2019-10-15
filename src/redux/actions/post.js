b4ce8e79d9a87ff0786f65f5e7cf780806ad1331
sweetmate-dev <john.amuesi525@gmail.com>
2019-10-15 15:01:01 +0100

    update

--- src/redux/actions/post.js
+++ src/redux/actions/post.js
@@ -1,11 +1,10 @@
 import * as types from './types';
 
-//title, location, date
 export const updatePostStintData = (data) => ({
   type: types.SET_POST_STINT_DATA,
   payload: data,
 })
-//instruction and uniform
+
 export const savePostStintInfo = (data) => ({
   type: types.SET_POST_STINT_INFO,
   payload: data,
